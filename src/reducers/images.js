ca9461595040ca6d5486fd3a881528eb9358604f
Rem Zolotykh <rem@zolotykh.net>
2017-03-21 23:06:32 +0800

    首页图片更新

--- src/reducers/images.js
+++ src/reducers/images.js
@@ -1,32 +1,32 @@
   const initialState = [
     {
-      tagimage: require('../../images/sources/tagimage1/tagimage1.jpeg'),
+      tagimage: require('../../images/sources/dr/index.jpg'),
+      tagName: 'DR',
+      imagelist: []
+    },
+    {
+      tagimage: require('../../images/sources/routine/index.jpg'),
       tagName: 'routine',
       imagelist: []
     },
     {
-      tagimage: require('../../images/sources/tagimage2/tagimage2.jpeg'),
-      tagName: 'web',
-      imagelist: []
-    },
-    {
-      tagimage: require('../../images/sources/tagimage3/tagimage3.jpeg'),
+      tagimage: require('../../images/sources/app/index.jpg'),
       tagName: 'app',
       imagelist: []
     },
     {
-      tagimage: require('../../images/sources/tagimage4/tagimage4.jpeg'),
+      tagimage: require('../../images/sources/freehand/index.jpg'),
       tagName: 'freehand',
       imagelist: []
     },
     {
-      tagimage: require('../../images/sources/tagimage5/tagimage5.jpeg'),
+      tagimage: require('../../images/sources/picture/index.jpg'),
       tagName: 'picture',
       imagelist: []
     },
     {
-      tagimage: require('../../images/sources/tagimage6/tagimage6.jpeg'),
-      tagName: 'haha',
+      tagimage: require('../../images/sources/web/index.jpg'),
+      tagName: 'web',
       imagelist: []
     }
   ]
